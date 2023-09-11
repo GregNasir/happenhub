9bc1496af7919b6d522324b3c39b7ab481fdb0dd
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-09-10 21:09:31 -0400

    react

--- models/User.jsx
+++ models/User.jsx
@@ -17,24 +17,7 @@ const userSchema = new Schema(
             "Please fill a valid email address",
         ],
 }
-    //     thoughts: [
-    //     {
-    //         type: Schema.Types.ObjectId,
-    //         ref: "Thought",
-    //     },
-    //     ],
-    //     friends: [
-    //     {
-    //         type: Schema.Types.ObjectId,
-    //         ref: "User",
-    //     },
-    //     ],
-    // },
-    // {
-    //     toJSON: {
-    //     virtuals: true,
-    //     },
-    //     id: false,
+
     }
     );
 
