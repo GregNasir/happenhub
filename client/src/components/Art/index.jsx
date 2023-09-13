ed50baf029420303adebb3cbda5beba7394f4dec
delia <you@example.com>
2023-09-12 20:41:17 -0400

    pushing up code to sports, about, music, food, art, and index.css

--- client/src/components/Art/index.jsx
+++ client/src/components/Art/index.jsx
@@ -1,14 +1,53 @@
+//import React from 'react';
+import art1 from '../../assets/images/art1.jpg';
+import art2 from '../../assets/images/art2.jpg';
+import art3 from '../../assets/images/art3.jpg';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
 import './index.scss';
 
+    function Art() {
+            return (
+                <CardGroup>
+                    <Card>
+                    <Card.Img variant="top" src={art1} />
+                    <Card.Body>
+                        <Card.Title>The Modern Art Museum</Card.Title>
+                        <Card.Text>
+                        Located in Midtown Manhattan, the Museum of Modern Art (MoMA) is home to the worlds greatest collection of modern and contemporary art. From the painting and sculpture of the 1880s to the film, design, and performance art of today, MoMA provides inspiration for visitors of all ages.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="primary">Select</Button>
+                    </Card.Footer>
+                    </Card>
+                    <Card>
+                    <Card.Img variant="top" src={art2} />
+                    <Card.Body>
+                        <Card.Title>Princeton University Art Museum</Card.Title>
+                        <Card.Text>
+                        Located in Princeton, New Jersey, the Princeton University Art Museum is one of the leading university art museums in the country, with collections of more than 100,000 works of art ranging from ancient to contemporary art and spanning the globe. The Museum is free and open to the public year-round.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="primary">Select</Button>
+                    </Card.Footer>
+                    </Card>
+                    <Card>
+                    <Card.Img variant="top" src={art3} />
+                    <Card.Body>
+                        <Card.Title>Museum of Comtemporary Art</Card.Title>
+                        <Card.Text>
+                        LACMA, the largest art museum in the western United States, is located in the heart of Los Angeles, California. It holds more than 150,000 works spanning the history of art from ancient times to the present. In addition to art exhibits, the museum features film and concert series.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="primary">Select</Button>
+                    </Card.Footer>
+                    </Card>
+                </CardGroup>
+            );
+        }
 
-const Art = () => {
-    return (
-        <>
-        
-            <h1>Art Events</h1>
-        
-        </>
-    )
-}
-
-export default Art;
+    export default Art;
--- client/src/components/Sports/index.jsx
+++ client/src/components/Sports/index.jsx
@@ -1,14 +1,52 @@
+import basketball from '../../assets/images/basketball.jpg';
+import hockey from '../../assets/images/hockey.jpg';
+import boxing from '../../assets/images/boxing.jpg';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
 import './index.scss';
 
+    function Sports() {
+            return (
+                <CardGroup>
+                    <Card>
+                    <Card.Img variant="top" src={basketball} />
+                    <Card.Body>
+                        <Card.Title>Basketball Games</Card.Title>
+                        <Card.Text>
+                        Tip-Off Time: Get Ready to Slam Dunk into Basketball Action! Enjoy the excitement of the NBA or College games, and the best players in the world.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="primary">Select</Button>
+                    </Card.Footer>
+                    </Card>
+                    <Card>
+                    <Card.Img variant="top" src={hockey} />
+                    <Card.Body>
+                        <Card.Title>Hockey Games</Card.Title>
+                        <Card.Text>
+                        Get Ready to Rumble: Hockey Games that Score Big! Enjoy the excitement of the NHL or College games, and the best players in the world.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="primary">Select</Button>
+                    </Card.Footer>
+                    </Card>
+                    <Card>
+                    <Card.Img variant="top" src={boxing} />
+                    <Card.Body>
+                        <Card.Title>Boxing</Card.Title>
+                        <Card.Text>
+                        Step into the Ring: An Evening of Championship Boxing. Enjoy the excitement of the best boxers in the world.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="primary">Select</Button>
+                    </Card.Footer>
+                    </Card>
+                </CardGroup>
+            );
+        }
 
-const Sports = () => {
-    return (
-        <>
-        
-            <h1>Sports Events</h1>
-        
-        </>
-    )
-}
-
-export default Sports;
+    export default Sports;
