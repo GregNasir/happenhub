ce6be5a3beef165cd7420b1a897cb8d3a7a15eb1
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-09-14 17:43:37 -0500

    edited user

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,36 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types, DataTypes } = require('mongoose');
 
 const userSchema = new Schema(
     {
+        id: {
+            type: Number,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+                },
+
         username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
         },
-        email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Please fill a valid email address",
-        ],
-}
+            email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please fill a valid email address",
+            ],
+            },
+        password: {
+                type: String,
+                allowNull: false,
+                validate: {
+                    len: [8],
+        },
+        },  
 
     }
     );
