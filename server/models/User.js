540ce2897da1ce92ed9556cad7084d4729fb39e3
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-09-14 18:22:51 -0500

    Revert "edited user"
    This reverts commit ce6be5a3beef165cd7420b1a897cb8d3a7a15eb1.
    
    trying to get back to a working server

--- server/models/User.js
+++ server/models/User.js
@@ -1,36 +1,22 @@
-const { Schema, model, Types, DataTypes } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        id: {
-            type: Number,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-                },
-
         username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
         },
-            email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [
-                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                "Please fill a valid email address",
-            ],
-            },
-        password: {
-                type: String,
-                allowNull: false,
-                validate: {
-                    len: [8],
-        },
-        },  
+        email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please fill a valid email address",
+        ],
+}
 
     }
     );
