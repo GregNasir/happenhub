04d07052bce3f8ac93284fb7efa5225ce65b47cb
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-09-20 00:02:18 -0400

    added dist folder

--- server/server.js
+++ server/server.js
@@ -240,10 +240,10 @@ const startApolloServer = async () => {
 
   // Serve static files in production
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
